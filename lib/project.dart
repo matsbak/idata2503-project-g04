46718eae67c814939542fc0f24b5513b255fe856
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-09-20 12:59:37 +0200

    Added base widgets

---
+++ lib/project.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Project extends StatelessWidget {
+  const Project({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text('Movie rater'),
+      ),
+    );
+  }
+}
