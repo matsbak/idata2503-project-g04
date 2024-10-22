70f7c2cc8624c1f1e6b1f13391cda960d77be825
Sandegr <sander.grimstad00@gmail.com>
2024-10-22 13:15:45 +0200

    Added more dummy movies

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -2,7 +2,7 @@ import 'package:project/models/movie.dart';
 
 var dummyMovies = [
   Movie(
-    title: 'Cars',
+    title: 'Cars 2',
     description:
         'On the way to the biggest race of his life, a hotshot rookie race car gets stranded in a rundown town and learns that winning isn\'t everything in life.',
     rating: 5.0,
@@ -72,5 +72,59 @@ var dummyMovies = [
     genre: Genre.scienceFiction,
     posterUrl:
         'https://m.media-amazon.com/images/M/MV5BNWIyNmU5MGYtZDZmNi00ZjAwLWJlYjgtZTc0ZGIxMDE4ZGYwXkEyXkFqcGc@._V1_.jpg',
+  ),
+  Movie(
+    title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
+    description:
+        'Blacksmith Will Turner teams up with eccentric pirate "Captain" Jack Sparrow to save Elizabeth Swann, the governors daughter and his love, from Jacks former pirate allies, who are now undead.',
+    rating: 4.0,
+    genre: Genre.fantasy,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BNDhlMzEyNzItMTA5Mi00YWRhLThlNTktYTQyMTA0MDIyNDEyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+  ),
+  Movie(
+    title: 'Harry Potter and the Philosophers Stone',
+    description:
+        'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.',
+    rating: 4.0,
+    genre: Genre.fantasy,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BNTU1MzgyMDMtMzBlZS00YzczLThmYWEtMjU3YmFlOWEyMjE1XkEyXkFqcGc@._V1_.jpg',
+  ),
+  Movie(
+    title: 'It',
+    description:
+        'In the summer of 1989, a group of bullied kids band together to destroy a shape-shifting monster, which disguises itself as a clown and preys on the children of Derry, their small Maine town.',
+    rating: 4.0,
+    genre: Genre.horror,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BZGZmOTZjNzUtOTE4OS00OGM3LWJiNGEtZjk4Yzg2M2Q1YzYxXkEyXkFqcGc@._V1_.jpg',
+  ),
+  Movie(
+    title: 'Die Hard',
+    description:
+        'A New York City police officer tries to save his estranged wife and several others taken hostage by terrorists during a Christmas party at the Nakatomi Plaza in Los Angeles.',
+    rating: 4.5,
+    genre: Genre.action,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BMGNlYmM1NmQtYWExMS00NmRjLTg5ZmEtMmYyYzJkMzljYWMxXkEyXkFqcGc@._V1_.jpg',
+  ),
+  Movie(
+    title: 'Scary Movie',
+    description:
+        'A year after disposing of the body of a man they accidentally killed, a group of dumb teenagers are stalked by a bumbling serial killer.',
+    rating: 3.5,
+    genre: Genre.comedy,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BZGRmMGRhOWMtOTk3Ni00OTRjLTkyYTAtYzA1M2IzMGE3NGRkXkEyXkFqcGc@._V1_.jpg',
+  ),
+  Movie(
+    title: 'Interstellar',
+    description:
+        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
+    rating: 5.0,
+    genre: Genre.scienceFiction,
+    posterUrl:
+        'https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg',
   ),
 ];
