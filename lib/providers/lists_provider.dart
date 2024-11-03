02f7de47f33cc42f243e797c5baa877c7a74efed
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-11-03 15:15:00 +0100

    Added further state management for adding movies to lists

--- lib/providers/lists_provider.dart
+++ lib/providers/lists_provider.dart
@@ -8,9 +8,30 @@ class WatchlistNotifier extends StateNotifier<List<Movie>> {
   void addToWatchlist(Movie movie) {
     state = [...state, movie];
   }
+
+  void removeFromWatchlist(Movie movie) {
+    state = state.where((m) => m != movie).toList();
+  }
+}
+
+class MyListNotifier extends StateNotifier<List<Movie>> {
+  MyListNotifier() : super([]);
+
+  void addToMyList(Movie movie) {
+    state = [...state, movie];
+  }
+
+  void removeFromMyList(Movie movie) {
+    state = state.where((m) => m != movie).toList();
+  }
 }
 
 final watchlistProvider =
     StateNotifierProvider<WatchlistNotifier, List<Movie>>((ref) {
   return WatchlistNotifier();
+});
+
+final myListProvider =
+    StateNotifierProvider<MyListNotifier, List<Movie>>((ref) {
+  return MyListNotifier();
 });
