88b81ee9edba3117aa5405139feef9dbf8fcbe48
Sandegr <sander.grimstad00@gmail.com>
2024-11-19 16:04:05 +0100

    Added Firebase service class
    With methods to add and remove movies to/from firebase

--- lib/screens/movie_detail_screen.dart
+++ lib/screens/movie_detail_screen.dart
@@ -8,6 +8,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:project/models/movie.dart';
 import 'package:project/providers/lists_provider.dart';
+import 'package:project/service/FirebaseService.dart';
 import 'package:project/widgets/reviews.dart';
 import 'package:project/widgets/starbuilder.dart';
 
@@ -78,17 +79,36 @@ class _MovieDetailScreenState extends ConsumerState<MovieDetailScreen> {
   }
 
   /// Adds a movie to the watch list
-  void _addToWatchList(BuildContext context, WidgetRef ref) {
-    ref.read(watchlistProvider.notifier).addToWatchlist(widget.movie);
-    ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Movie added to watchlist')));
+  void _addToWatchList(BuildContext context, WidgetRef ref) async {
+    try {
+      final firebaseKey =
+          await FirebaseService.addMovieToFirebase(widget.movie);
+      if (firebaseKey != null) {
+        ref.read(watchlistProvider.notifier).addToWatchlist(widget.movie);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Movie added to watchlist')),
+        );
+      }
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to add movie to watchlist: $error")),
+      );
+    }
   }
 
   /// Removes a movie from the watch list
-  void _removeFromWatchList(BuildContext context, WidgetRef ref) {
-    ref.read(watchlistProvider.notifier).removeFromWatchlist(widget.movie);
-    ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Movie removed from watchlist')));
+  void _removeFromWatchList(BuildContext context, WidgetRef ref) async {
+    try {
+      await FirebaseService.removeMovieById(widget.movie.id);
+
+      ref.read(watchlistProvider.notifier).removeFromWatchlist(widget.movie);
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Movie removed from watchlist')));
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to remove moive to watchlist: $error"),
+      ));
+    }
   }
 
   @override
