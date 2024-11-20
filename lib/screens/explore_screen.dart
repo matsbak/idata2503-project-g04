192953f31d82e36b8ac15624058bc61831005513
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-11-20 14:47:29 +0100

    Refactored explore screen to use movie data from API

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -5,7 +5,7 @@ import 'package:project/widgets/explore_list_item.dart';
 
 /// A screen representing the explore section of the app. The explore screen
 /// is to show the user with the different movies based on category.
-class ExploreScreen extends StatefulWidget {
+class ExploreScreen extends StatelessWidget {
   const ExploreScreen({
     super.key,
     required this.movies,
@@ -14,41 +14,43 @@ class ExploreScreen extends StatefulWidget {
   final List<Movie> movies;
 
   @override
-  State<ExploreScreen> createState() {
-    return _ExploreScreenState();
-  }
-}
-
-class _ExploreScreenState extends State<ExploreScreen> {
-  @override
   Widget build(BuildContext context) {
-    // final actionMovies =
-    //     dummyMovies.where((movie) => movie.genre == Genre.action).toList();
-    // final fantacyMovies =
-    //     dummyMovies.where((movie) => movie.genre == Genre.fantasy).toList();
-    // final sciFiMovies = dummyMovies
-    //     .where((movie) => movie.genre == Genre.scienceFiction)
-    //     .toList();
-    // final comedyMovies =
-    //     dummyMovies.where((movie) => movie.genre == Genre.comedy).toList();
-    // final horrorMovies =
-    //     dummyMovies.where((movie) => movie.genre == Genre.horror).toList();
-    // final dramaMovies =
-    //     dummyMovies.where((movie) => movie.genre == Genre.drama).toList();
+    List<String> genres = [];
+
+    // Initializes all genres to be shown on the screen
+    for (final movie in movies) {
+      for (String genre in movie.genres) {
+        if (!genres.contains(genre)) {
+          genres.add(genre);
+        }
+      }
+    }
+
+    List<Movie> getGenreMovies(String genre) {
+      List<Movie> genreMovies = [];
+
+      for (final movie in movies) {
+        if (movie.genres.contains(genre)) {
+          genreMovies.add(movie);
+        }
+      }
+
+      return genreMovies;
+    }
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ListView(
         children: [
-          // _buildGenreSection(genre),
+          for (final genre in genres)
+            _buildGenreSection(genre, getGenreMovies(genre)),
         ],
       ),
     );
   }
 
   /// A widget that builds a genre section with every movie in that given genre.
-  // Widget buildGenreSection(Genre genre, List<Movie> movies) {
-  Widget _buildGenreSection(String genre) {
+  Widget _buildGenreSection(String genre, List<Movie> genreMovies) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -67,9 +69,9 @@ class _ExploreScreenState extends State<ExploreScreen> {
           height: 200,
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: widget.movies.length,
+              itemCount: genreMovies.length,
               itemBuilder: (ctx, index) {
-                return ExploreListItem(movie: widget.movies[index]);
+                return ExploreListItem(movie: genreMovies[index]);
               }),
         ),
       ],
