29d4783a21b3265b330c16a325f0c278bf2d0215
Sandegr <sander.grimstad00@gmail.com>
2024-11-21 09:38:09 +0100

    Fixed overflow error on modal

--- lib/widgets/lists/watchlist/watchlist_modal.dart
+++ lib/widgets/lists/watchlist/watchlist_modal.dart
@@ -62,148 +62,147 @@ class _WatchlistModalState extends ConsumerState<WatchlistModal> {
       padding: const EdgeInsets.symmetric(
         horizontal: 16.0,
       ),
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.9,
+      ),
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(
-              Icons.close,
-              size: 28.0,
-              color: Colors.white,
-            ),
-          ),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.only(
-                top: 10.0,
-                bottom: 24.0,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: SizedBox(
-                width: 250.0,
-                child: widget.movie.poster,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(
+                Icons.close,
+                size: 28.0,
+                color: Colors.white,
               ),
             ),
-          ),
-          Text(
-            widget.movie.title,
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 10.0,
+                  bottom: 24.0,
                 ),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Row(
-            children: [
-              StarBuilder(rating: widget.movie.averageRating),
-              const SizedBox(
-                width: 4.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: SizedBox(
+                  width: 250.0,
+                  child: widget.movie.poster,
+                ),
               ),
-              Text(
-                widget.movie.averageRating.toString(),
+            ),
+            Text(
+              widget.movie.title,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Row(
+              children: [
+                StarBuilder(rating: widget.movie.averageRating),
+                const SizedBox(
+                  width: 4.0,
+                ),
+                Text(
+                  widget.movie.averageRating.toString(),
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              widget.movie.description,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Text('Done watching?',
                 style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                       color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+            const SizedBox(
+              height: 8.0,
+            ),
+            FilledButton(
+              onPressed: isAddedToMyList
+                  ? () {
+                      _removeFromMyList();
+                    }
+                  : () {
+                      _addToMyList(widget.movie.title);
+                    },
+              child: SizedBox(
+                width: 125.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    isAddedToMyList
+                        ? const Icon(
+                            Icons.check,
+                          )
+                        : const Icon(
+                            Icons.add,
+                          ),
+                    const SizedBox(
+                      width: 2.0,
                     ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          SizedBox(
-            height: 112.0,
-            child: Expanded(
-              child: SingleChildScrollView(
-                child: Text(
-                  widget.movie.description,
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.onPrimaryContainer),
+                    const Text(
+                      'Add to my list',
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          Text('Done watching?',
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )),
-          const SizedBox(
-            height: 8.0,
-          ),
-          FilledButton(
-            onPressed: isAddedToMyList
-                ? () {
-                    _removeFromMyList();
-                  }
-                : () {
-                    _addToMyList(widget.movie.title);
-                  },
-            child: SizedBox(
-              width: 125.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  isAddedToMyList
-                      ? const Icon(
-                          Icons.check,
-                        )
-                      : const Icon(
-                          Icons.add,
-                        ),
-                  const SizedBox(
-                    width: 2.0,
-                  ),
-                  const Text(
-                    'Add to my list',
-                  ),
-                ],
+            Container(
+              margin: const EdgeInsets.symmetric(
+                vertical: 10.0,
               ),
-            ),
-          ),
-          const Spacer(),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: 10.0,
-            ),
-            child: FilledButton.tonal(
-              onPressed: () {
-                _removeFromWatchlist(widget.movie.title);
-              },
-              style: FilledButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.errorContainer,
-              ),
-              child: const Row(
-                children: [
-                  Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 2.0,
-                  ),
-                  Text(
-                    'Remove',
-                    style: TextStyle(
+              child: FilledButton.tonal(
+                onPressed: () {
+                  _removeFromWatchlist(widget.movie.title);
+                },
+                style: FilledButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.errorContainer,
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.delete,
                       color: Colors.white,
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      width: 2.0,
+                    ),
+                    Text(
+                      'Remove',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
