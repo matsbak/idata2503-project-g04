ef59b4a3383802938872c602116ca996ea45cbc2
Sandegr <sander.grimstad00@gmail.com>
2024-11-21 09:40:01 +0100

    Can now add a review to the movie from modal

--- lib/widgets/lists/my_list/my_list_modal.dart
+++ lib/widgets/lists/my_list/my_list_modal.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project/models/movie.dart';
 import 'package:project/models/rating.dart';
 import 'package:project/providers/lists_provider.dart';
+import 'package:project/service/firebase_service.dart';
 import 'package:project/widgets/starbuilder.dart';
 
 // TODO Make main content area scrollable
@@ -23,6 +24,7 @@ class _MyListModalState extends ConsumerState<MyListModal> {
   final _formKey = GlobalKey<FormState>();
   var _enteredScore = 0.0;
   var _enteredReview = '';
+  final _userId = 'test User';
 
   void _removeFromMyList(String title) {
     ref.read(myListProvider.notifier).removeFromMyList(widget.movie);
@@ -35,19 +37,25 @@ class _MyListModalState extends ConsumerState<MyListModal> {
     ));
   }
 
-  void _saveRating() {
+  /// Saves user input and adds review to backend.
+  void _saveRating() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-
       final newRating = Rating(
         score: _enteredScore,
         review: _enteredReview,
-        userId: 'Test user',
+        userId: _userId,
         date: DateTime.now(),
       );
-      widget.movie.addReview(newRating);
 
-      Navigator.of(context).pop();
+      try {
+        await FirebaseService.addRatingToMovie(widget.movie.id, newRating);
+        Navigator.of(context).pop(newRating);
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to save rating: $error')),
+        );
+      }
     }
   }
 
@@ -60,190 +68,194 @@ class _MyListModalState extends ConsumerState<MyListModal> {
       padding: const EdgeInsets.symmetric(
         horizontal: 16.0,
       ),
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.9,
+      ),
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.close,
-              size: 28.0,
-              color: Colors.white,
-            ),
-          ),
-          Center(
-            child: Container(
-              margin: const EdgeInsets.only(
-                top: 10.0,
-                bottom: 24.0,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: SizedBox(
-                width: 250.0,
-                child: widget.movie.poster,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.close,
+                size: 28.0,
+                color: Colors.white,
               ),
             ),
-          ),
-          Text(
-            widget.movie.title,
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 10.0,
+                  bottom: 24.0,
                 ),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Row(
-            children: [
-              StarBuilder(rating: _enteredScore),
-              const SizedBox(
-                width: 4.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: SizedBox(
+                  width: 250.0,
+                  child: widget.movie.poster,
+                ),
               ),
-              Text(
-                _enteredScore.toString(),
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: Colors.white,
-                    ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            Text(
+              widget.movie.title,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Row(
               children: [
-                Text(
-                  'Score',
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
+                StarBuilder(rating: _enteredScore),
                 const SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  keyboardType: TextInputType.number,
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.white,
-                      ),
-                  initialValue: _enteredScore.toString(),
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        value.length > 3 ||
-                        double.tryParse(value) == null ||
-                        double.tryParse(value)! <= 0.0 ||
-                        double.tryParse(value)! > 5.0 ||
-                        double.tryParse(value)! % 0.5 != 0) {
-                      return 'Must be a half-decimal value between 0.0 and 5.0';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    if (value.isNotEmpty &&
-                        value.length <= 3 &&
-                        double.tryParse(value)! > 0.0 &&
-                        double.tryParse(value)! <= 5.0 &&
-                        double.tryParse(value)! % 0.5 == 0) {
-                      setState(() {
-                        _enteredScore = double.parse(value);
-                      });
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 16.0,
+                  width: 4.0,
                 ),
                 Text(
-                  'Add a review?',
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  maxLength: 100,
-                  maxLines: 3,
+                  _enteredScore.toString(),
                   style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                         color: Colors.white,
                       ),
-                  validator: (value) {
-                    if (value == null || value.trim().length > 100) {
-                      return 'Must be at most 100 characters';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _enteredReview = value!;
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        _removeFromMyList(widget.movie.title);
-                      },
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                      style: IconButton.styleFrom(
-                        backgroundColor:
-                            Theme.of(context).colorScheme.errorContainer,
-                      ),
-                    ),
-                    const Spacer(),
-                    FilledButton.tonal(
-                      onPressed: () => _formKey.currentState!.reset(),
-                      style: FilledButton.styleFrom(
-                        foregroundColor: Colors.white,
-                      ),
-                      child: const Text('Clear'),
-                    ),
-                    const SizedBox(
-                      width: 8.0,
-                    ),
-                    FilledButton(
-                      onPressed: _saveRating,
-                      child: const Text(
-                        'Save',
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(height: 12.0),
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Score',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                        ),
+                    initialValue: _enteredScore.toString(),
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.length > 3 ||
+                          double.tryParse(value) == null ||
+                          double.tryParse(value)! <= 0.0 ||
+                          double.tryParse(value)! > 5.0 ||
+                          double.tryParse(value)! % 0.5 != 0) {
+                        return 'Must be a half-decimal value between 0.0 and 5.0';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      if (value.isNotEmpty &&
+                          value.length <= 3 &&
+                          double.tryParse(value)! > 0.0 &&
+                          double.tryParse(value)! <= 5.0 &&
+                          double.tryParse(value)! % 0.5 == 0) {
+                        setState(() {
+                          _enteredScore = double.parse(value);
+                        });
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Text(
+                    'Add a review?',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    maxLength: 100,
+                    maxLines: 3,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: Colors.white,
+                        ),
+                    validator: (value) {
+                      if (value == null || value.trim().length > 100) {
+                        return 'Must be at most 100 characters';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredReview = value!;
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _removeFromMyList(widget.movie.title);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                        style: IconButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.errorContainer,
+                        ),
+                      ),
+                      const Spacer(),
+                      FilledButton.tonal(
+                        onPressed: () => _formKey.currentState!.reset(),
+                        style: FilledButton.styleFrom(
+                          foregroundColor: Colors.white,
+                        ),
+                        child: const Text('Clear'),
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      FilledButton(
+                        onPressed: _saveRating,
+                        child: const Text(
+                          'Save',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
