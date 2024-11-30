084c3cda6683b31392241476e458832938e4587c
Sandegr <sander.grimstad00@gmail.com>
2024-11-30 12:22:45 +0100

    Removed redundant code

--- lib/screens/lists.dart
+++ lib/screens/lists.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:project/providers/lists_provider.dart';
-import 'package:project/widgets/lists/lists_button.dart';
 import 'package:project/widgets/lists/my_list/my_list.dart';
 import 'package:project/widgets/lists/watchlist/watchlist.dart';
 
@@ -16,27 +15,6 @@ class ListsScreen extends ConsumerStatefulWidget {
 }
 
 class _ListsScreenState extends ConsumerState<ListsScreen> {
-  String _currentList = 'watchlist';
-
-  bool _isWatchlistActive = true;
-  bool _isMyListActive = false;
-
-  void switchToWatchlist() {
-    setState(() {
-      _currentList = 'watchlist';
-      _isWatchlistActive = true;
-      _isMyListActive = false;
-    });
-  }
-
-  void switchToMyList() {
-    setState(() {
-      _currentList = 'mylist';
-      _isWatchlistActive = false;
-      _isMyListActive = true;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
